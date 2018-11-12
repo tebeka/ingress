d05025f82efa4bef85dc90d800943ef84135f9bb
Miki Tebeka <miki.tebeka@gmail.com>
2018-11-12 21:50:06 +0000

    read version and github URL

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-import ingress
+
+
+def version():
+    with open('ingress.py') as fp:
+        for line in fp:
+            if '__version__' not in line:
+                continue
+            _, ver = line.split('=')
+            return ver.replace("'", '').strip()
+
 
 setup(
     name='ingress',
-    version=ingress.__version__,
+    version=version(),
     description='A back door to servers.',
-    long_description=ingress.__doc__,
     author='Miki Tebeka',
     author_email='miki.tebeka@gmail.com',
-    url='https://bitbucket.org/tebeka/ingress/src',
+    url='https://github.com/tebeka/ingress',
     license='MIT License',
     platforms=['any'],
     zip_safe=True,
     py_modules=['ingress'],
     install_requires=['six'],
 )
-
